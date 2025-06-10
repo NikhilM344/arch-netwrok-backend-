e1b7b2955a92e6e55f96eedb36f0bc44c5a281a4
kapil9179 <kapilpandeyweb@gmail.com>
2025-06-10 20:03:37 +0530

    add welcome message on / url

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import enviormentConfig from './configs/enviorment.js';
-import connectDb from './configs/dbconfig.js';
-import userRegistration from './router/auth/userauth.js'
-import vendorRegistration from './router/auth/vendorauth.js'
+import express from "express";
+import cors from "cors";
+import enviormentConfig from "./configs/enviorment.js";
+import connectDb from "./configs/dbconfig.js";
+import userRegistration from "./router/auth/userauth.js";
+import vendorRegistration from "./router/auth/vendorauth.js";
 
 const app = express();
 connectDb();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-
-app.use('/userauth',userRegistration);
-app.use("/auth",vendorRegistration);
-app.listen(enviormentConfig.port,()=>{
-    console.log(`server is running on port ${enviormentConfig.port}`)
-})
+app.use("/", (req, res) => {
+  res.json({ message: "Welcome To Build Query" });
+});
+app.use("/userauth", userRegistration);
+app.use("/auth", vendorRegistration);
+app.listen(enviormentConfig.port, () => {
+  console.log(`server is running on port ${enviormentConfig.port}`);
+});
