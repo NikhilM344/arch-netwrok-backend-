dc386d678fd624ff29e31e7cbaf47d764125e18c
kapil9179 <kapilpandeyweb@gmail.com>
2025-06-11 21:44:17 +0530

    add cors policy in backend

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import vendorRegistration from "./router/auth/vendorauth.js";
 
 const app = express();
 connectDb();
-app.use(cors());
+app.use(cors({
+  origin: ['http://buildquery.com', 'http://www.buildquery.com'],
+  credentials: true,
+}));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Build Query API is live 🚀" });
