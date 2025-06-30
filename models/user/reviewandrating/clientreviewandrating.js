551e8a59ea4af295a779c35252d65ff25183bfb2
kapil9179 <kapilpandeyweb@gmail.com>
2025-06-30 23:46:46 +0530

    add review schema with compound indexing

---
+++ models/user/reviewandrating/clientreviewandrating.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+  clientId:{
+   type:mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref:"user"
+  },
+  professionlId:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref:"Vendor"
+  },
+  rating:{
+    type:Number,
+    required:true,
+    min:1,
+    max:5,
+  },
+  comment:{
+    type:String,
+    required:false,
+    maxlength:[400,"comment allowed only 400 characters"],
+    minlength:[10,"atleast comment should be 10 characters or long 10 characters"]
+  }
+},{timestamps:true});
+
+// Prevent duplicate reviews from same user
+reviewSchema.index({ user: 1, professional: 1 }, { unique: true });
+
+export const reviewModel = mongoose.model('reviews',reviewSchema);
