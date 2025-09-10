a678233e2db4bec74a895e347c7f7c0d0e65b734
kapil9179 <kapilpandeyweb@gmail.com>
2025-09-11 02:02:27 +0530

    changes on static file serve

--- index.js
+++ index.js
@@ -17,26 +17,12 @@ import adminProfRoutes from "./router/admin/professional/adminprofRoutes.js";
 import otpVerificationRoutes from "./router/auth/otp/otpverificationroute.js";
 import proDashboardInfoRoutes from "./router/professional/dashboard/dashboardRoutes.js";
 import newRegistrationRoute from "./router/auth/newprofessionalauth.js";
-import deleteProfessionalAccountRouter from './router/professional/account/deleteprofaccountrouter.js'
-import userDashboardDetailRouter from './router/user/dashboard/userdashboarddetailrouter.js'
+import deleteProfessionalAccountRouter from "./router/professional/account/deleteprofaccountrouter.js";
+import userDashboardDetailRouter from "./router/user/dashboard/userdashboarddetailrouter.js";
 import "./utility/mail/schdulemail.js";
 
 const app = express();
 connectDb();
-// app.use(
-//   cors({
-//     origin: [
-//       "http://buildquery.com",
-//       "http://www.buildquery.com",
-//       "http://localhost:8080",
-//       "http://localhost:8081",
-//       "http://localhost:8082",
-//       "http://admin.buildquery.com",
-//     ],
-//     credentials: true,
-//   })
-// );
-
 const allowedOrigins = [
   "https://www.buildquery.com",
   "https://buildquery.com",
@@ -61,25 +47,26 @@ app.use(
   })
 );
 
-
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(express.json({ limit: "20mb" }));
-
-app.get("/test-env", (req, res) => {
-  res.json({ baseURL: enviormentConfig.backendBaseUrl });
-});
 
 app.get("/", (req, res) => {
   res.json({ message: "Build Query API is live 🚀" });
 });
 app.use("/admin", adminRoutes, adminClientRoutes, adminProfRoutes);
-app.use("/api", reviewRouter, updateProfileRouter, professionaldetailsRouter,deleteProfessionalAccountRouter);
-app.use("/uploads", express.static("uploads"));
+app.use(
+  "/api",
+  reviewRouter,
+  updateProfileRouter,
+  professionaldetailsRouter,
+  deleteProfessionalAccountRouter
+);
+app.use("/api/uploads", express.static("uploads"));
 app.use("/userauth", userRegistration);
 app.use("/auth", otpVerificationRoutes, newRegistrationRoute);
 app.use("/professional", professionList, proDashboardInfoRoutes);
 app.use("/request", clientRequest);
-app.use("/user", userprofile,userDashboardDetailRouter);
+app.use("/user", userprofile, userDashboardDetailRouter);
 app.use("/portfolio", professionalPortfolio);
 app.use("/profes", professionalProject);
 
