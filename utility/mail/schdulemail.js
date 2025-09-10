739e278bd2ce397e81644ad0ce8b60f220cb2dcc
kapil9179 <kapilpandeyweb@gmail.com>
2025-09-11 01:23:39 +0530

    changes on hardcoded http

--- utility/mail/schdulemail.js
+++ utility/mail/schdulemail.js
@@ -2,17 +2,18 @@ import cron from "node-cron";
 import { ScheduledReviewMail } from "../../models/mail/scheduledReviewMail.js";
 import sendMail from "./sendmail.js";
 import reviewRequestTemplate from "./templets/reviewmailtemp.js";
+import enviormentConfig from "../../configs/enviorment.js";
 
 cron.schedule("* * * * *", async () => { 
   const now = new Date();
   console.log("this cron job is running at", now);
 
   const mails = await ScheduledReviewMail.find({ sent: false, sendAt: { $lte: now } });
-  console.log("Mails to send:", mails.length);
+  
 
   for (const mail of mails) {
     try {
-      const reviewLink = `http://localhost:8080/review?professionalId=${mail.professionalId}&requestId=${mail.requestId}`;
+      const reviewLink = `&{enviormentConfig.backendBaseUrl}/review?professionalId=${mail.professionalId}&requestId=${mail.requestId}`;
       console.log("Sending mail to:", mail.email);
       await sendMail(
         mail.email,
@@ -22,6 +23,7 @@ cron.schedule("* * * * *", async () => {
       mail.sent = true;
       await mail.save();
       console.log("Mail sent and marked as sent for:", mail.email);
+      console.log("backend base url cron job",enviormentConfig.backendBaseUrl);
     } catch (err) {
       console.error("Failed to send scheduled review mail:", err);
     }
